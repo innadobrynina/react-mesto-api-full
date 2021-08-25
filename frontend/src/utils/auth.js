e61534f08d229262d7951e49747d74d39933c075
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-08-26 01:22:48 +0300

    change api url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://backend.indob.nomoredomains.monster';
+export const BASE_URL = 'https://auth.nomoreparties.co';
 
 export const checkStatus = (res) => {
     if (res.ok) {
