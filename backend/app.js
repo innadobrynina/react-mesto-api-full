8d06360d28a105359c30f6aa670e75ae3d3e316c
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-08-26 23:21:40 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,12 @@ const cardsRoutes = require('./routes/cards');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const allowedCors = [
+  'https://indob.nomoredomains.monster',
+  'http://indob.nomoredomains.monster',
+  'localhost:3000',
+];
+
 app.locals.jwtKey = 'secret-key';
 
 app.use(cookieParser());
@@ -30,6 +36,24 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
+
+  next();
 });
 
 app.use(express.urlencoded({ extended: true }));
