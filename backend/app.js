eaf8e8f65534ac34a32a74c30be63015b55b612c
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-08-27 23:43:29 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -58,31 +58,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use((req, res, next) => {
-  const { origin } = req.headers; // источник запроса - в переменную origin
-  const { method } = req; // тип запроса (HTTP-метод) в соотв. переменную
-  const requestHeaders = req.headers['access-control-request-headers']; // сохр. список заголовков исходного запроса
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  if (allowedCors.includes(origin)) {
-    // уст. заголовок, который разрешает браузеру запросы с этого источника
-    // console.log(origin);
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  // предварительный запрос? - добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими (requestHeaders) заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-
-  return next();
-});
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
