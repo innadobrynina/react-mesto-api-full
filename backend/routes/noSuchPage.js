d0ec5b50647efc359a35e780c33903cb5f0716ca
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-08-28 22:13:34 +0300

    правки

--- backend/routes/noSuchPage.js
+++ backend/routes/noSuchPage.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const NotFoundError = require('../errors/NotFoundError');
 
+// eslint-disable-next-line no-unused-vars
 router.all('*', (req, res) => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
