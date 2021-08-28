84b3c61f4d97243a255b44c1ca2ebb08d7c282d6
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-08-28 12:14:45 +0300

    правки

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,22 +3,21 @@ const AuthError = require('../errors/AuthError');
 
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const { NODE_ENV, JWT_SECRET = 'secret-key', JWT_DEV = 'dev-key' } = process.env;
   const token = req.cookies.jwt;
-  const secretKey = NODE_ENV === 'production' ? JWT_SECRET : JWT_DEV;
 
-  /* if (!token) {
+  if (!token) {
     return next(new AuthError('Необходимо авторизоваться'));
   }
 
-  let payload; */
+  let payload;
 
   try {
-    const payload = jwt.verify(token, secretKey);
-    req.user = payload; // записываем пейлоуд в объект запроса
+    payload = jwt.verify(token, req.app.locals.jwtKey);
   } catch (err) {
-    return new AuthError('Необходима авторизация');
+    return next(new AuthError('Неверный токен'));
   }
+
+  req.user = payload;
 
   next();
 };
